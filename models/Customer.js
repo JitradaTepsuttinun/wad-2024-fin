4b8450b115494dfdcd1c89521f4922e8b6b3efb9
JitradaTepsuttinun <u6410725@au.edu>
2024-10-11 09:39:28 +0700

    not much

--- models/Customer.js
+++ models/Customer.js
@@ -1,1 +1,24 @@
 import mongoose from "mongoose";
+
+const customerSchema = new mongoose.Schema({
+    name: {
+      type: String,
+      required: true
+    },
+    dob: {
+        type: Date,
+        required: false
+    },
+    number: {
+        type: Number,
+        required: true
+    },
+    interest: {
+        type: String
+    }
+  });
+  
+  const Customer = mongoose.models.customer || mongoose.model("customer", customerSchema);
+  
+  export default Customer;
+  
